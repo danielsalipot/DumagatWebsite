48e43fa8e47d61b87f914df61fd80295ed65654e
Resulynn <ian.ray.duyao@adamson.edu.ph>
2022-09-17 22:38:23 +0800

    index

---
+++ resources/views/inc/nav.blade.php
@@ -0,0 +1,42 @@
+<nav class="navbar navbar-expand-lg navconf fixed-top">
+  <div class="container-fluid">
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarTogglerDemo01">
+      <div class="d-flex w-100 text-decoration-none">
+
+        <div class="d-flex w-25 justify-content-start mt-3 navcontent">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="text-light text-decoration-none pe-5" href="#">Item</a>
+            </li>
+            <li class="nav-item">
+              <a class="text-light text-decoration-none" href="#">Item</a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="d-flex justify-content-center w-50">
+          <h1 class="navbar-brand header text-light" href="#">Dumagat</h1>
+        </div>
+
+        <div class="d-flex justify-content-end w-25 navcontent mt-3 ">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="text-light text-decoration-none pe-5" href="#">Item</a>
+            </li>
+            <li class="nav-item">
+              <a class="text-light text-decoration-none" href="#">Item</a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</nav>
+
+
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,30 @@
 @extends('layout.layout')
 
 @section('content')
-    Hello
+@include('inc.nav')
+
+<div class="w-100 d-flex justify-content-end head">
+    <div class="d-flex w-50">
+        <h3>The Dumagat<h3>
+    </div>
+</div>
+<div class="w-100 d-flex justify-content-end subtext">
+    <div class="d-flex w-50">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            <br>
+            <br>
+            <br>
+            <a href="#"> LEARN MORE</a>
+        </p>
+    </div>
+</div>
+
+<div class="d-flex">
+    <video autoplay muted loop id="myVideo" class="bgvid">
+        <source src="\assets\bg.mp4" type="video/mp4">
+    </video>
+</div>
+
+
 @endsection
