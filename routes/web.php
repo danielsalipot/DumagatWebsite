09226c5946914ae9cc263584d03f99693439e061
Daniel Salipot <danielsalipot@gmail.com>
2022-10-27 00:06:16 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\PagesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PagesController::class,'index']);
+Route::get('', [PagesController::class,'index']);
 Route::get('/about', [PagesController::class,'about']);
 Route::get('/gallery', [PagesController::class,'gallery']);
 Route::get('/language', [PagesController::class,'language']);
